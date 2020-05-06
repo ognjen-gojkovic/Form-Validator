860286ef591f95d5da74cf2818fc2bac2ae88b99
Ognjen Gojković <ognjen1804@gmail.com>
2020-05-06 23:54:59 +0200

    project-completed

--- src/js/index.js
+++ src/js/index.js
@@ -21,26 +21,62 @@ function showSuccess(input) {
 }
 
 // check email validility
-function isValid(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid.");
+  }
+}
+
+// get field name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// check required
+function checkRequired(inputArr) {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required.`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// function check validity
+function checkLenght(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters long.`
+    );
+  } else if (input.value.lenght > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less then ${max} characters long.`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match.");
+  }
 }
 
 // event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (username.value === "") {
-    showError(username, "Username is required!");
-  } else {
-    showSuccess(username);
-  }
-
-  if (email.value === "") {
-    showError(email, "Email is required!");
-  } else if (!isValid(email.value)) {
-    showError(email, "Email is not valid");
-  } else {
-    showSuccess(email);
-  }
+  checkRequired([username, email, password, password2]);
+  checkLenght(username, 3, 15);
+  checkLenght(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
